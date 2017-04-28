e0c94044429233f8979372c53f68c1f7f0c85b95
Christophe W <chris.wagn22@gmail.com>
2017-04-29 03:25:59 +0700

    Working

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 import { AppRegistry } from 'react-native'
 // import App from './App/Containers/App'
 import TodoApp from './components/todo-app';
-
 // To show network call in devtools
 GLOBAL.XMLHttpRequest = GLOBAL.originalXMLHttpRequest || GLOBAL.XMLHttpRequest
 

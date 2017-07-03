8b76f30e4d9d7db4bcac387f9a1a4660bd8555ed
Christophe W <chris.wagn22@gmail.com>
2017-07-03 15:04:42 +0200

    Fix back key

--- App/Containers/RootContainer.js
+++ App/Containers/RootContainer.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react'
-import { View, StatusBar } from 'react-native'
-import Navigation from '../Navigation/AppNavigation'
+import { View, StatusBar, BackHandler } from 'react-native'
 import { connect } from 'react-redux'
-import { addNavigationHelpers } from 'react-navigation'
+import { NavigationActions, addNavigationHelpers } from 'react-navigation'
+
+import Navigation from '../Navigation/AppNavigation'
 import StartupActions from '../Redux/StartupRedux'
 import ReduxPersist from '../Config/ReduxPersist'
 
@@ -21,6 +22,34 @@ class RootContainer extends Component {
     if (!ReduxPersist.active) {
       this.props.startup()
     }
+
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPress)
+  }
+
+  componentWillUnmount () {
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress)
+  }
+
+  hasNavigated = (nav) => {
+    if (nav.index > 0) return true
+
+    if (nav.routes) {
+      for (var i = 0; i < nav.routes.length; i++) {
+        if (this.hasNavigated(nav.routes[i])) return true
+      }
+    }
+
+    return false
+  }
+
+  onBackPress = () => {
+    const {dispatch, nav} = this.props
+    if (!this.hasNavigated(nav)) {
+      // Close app
+      return false
+    }
+    dispatch(NavigationActions.back())
+    return true
   }
 
   render () {

6da9982b77e3a571b2a1a22b210614340c251b56
Christophe Wagner <chris.wagn22@gmail.com>
2019-03-09 11:42:20 +0700

    Setup continuous integration

--- android/app/src/main/java/com/sleepolingo/MainApplication.java
+++ android/app/src/main/java/com/sleepolingo/MainApplication.java
@@ -3,6 +3,9 @@ package com.sleepolingo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
+import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
+import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -46,6 +49,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
+            new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
+            new AppCenterReactNativePackage(MainApplication.this),
             new RNFetchBlobPackage(),
             new RNGestureHandlerPackage(),
             new VectorIconsPackage(),
---
+++ ios/Pods/Target Support Files/Pods-SleepoLingoTests/Pods-SleepoLingoTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SleepoLingoTests : NSObject
+@end
+@implementation PodsDummy_Pods_SleepoLingoTests
+@end
